228f478b71984a9c646ae758dbff9be06dca62ba
Jason <Jason.patino94@gmail.com>
2022-01-03 09:27:07 -0500

    first commit

---
+++ enumerables.rb
@@ -0,0 +1,40 @@
+class Array
+
+    def my_each(&prc)
+        i = 0
+        while i < self.length
+            prc.call(self[i])
+            i += 1
+        end
+        self
+    end
+
+    def my_select(&prc)
+        selected = []
+
+        self.my_each do |ele|
+            selected << ele if prc.call(ele) == true
+        end
+    end
+
+end
+
+
+# ________________________________________________________________________________________________________
+
+puts "My Each"
+# My Each
+# calls my_each twice on the array, printing all the numbers twice.
+return_value = [1, 2, 3].my_each do |num|
+  puts num
+end.my_each do |num|
+  puts num
+end
+p return_value  # => [1, 2, 3]
+
+
+
+puts 'My Select'
+a = [1, 2, 3]
+p a.my_select { |num| num > 1 } # => [2, 3]
+p a.my_select { |num| num == 4 } # => []
